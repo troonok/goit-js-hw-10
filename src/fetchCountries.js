e7b201264d7721621477378224ab61df0d852100
Anastasiia Kandalintceva <valeryanaromanova@gmail.com>
2023-02-11 00:34:37 +0200

    проверка статуса запроса

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,10 @@ export function fetchCountries(name) {
 
     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
-        console.log(response)
+            if (!response.ok) {
+                throw new Error(response.status);
+              }
+              console.log(response)
         return response.json();
     })
 }

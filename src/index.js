5c26e2bc4533fcc27dc53a29c1900d6953a485b5
Anastasiia Kandalintceva <valeryanaromanova@gmail.com>
2023-02-11 18:56:20 +0200

    добавление разметки через функцию

--- src/index.js
+++ src/index.js
@@ -15,40 +15,55 @@ function onSearch(event) {
     const name = input.value.trim();
     cleanInput();
     if (name) {
-        fetchCountries(name)
-            .then(renderCounty)
-            .catch(error => { Notify.failure("Oops, there is no country with that name") })
-    } 
-}
-function cleanInput(){
-    list.innerHTML = ""
-        countryInfo.innerHTML=""
-        return
-}
-
-function renderCounty(country) {
-    if (country.length > 10) {
-        Notify.info("Too many matches found. Please enter a more specific name.");
-    } else if (country.length > 1 && country.length <= 10) {
-         
-        for (let i = 0; i < country.length; i += 1) {
-            const countriesName = country[i].name.official;
-            const flags = country[i].flags.svg;  
-            list.innerHTML += `<li class="country_header_big"><img src="${flags}" alt="flag of ${countriesName}" width="50" ><p class="country_name_big">${countriesName}</p>`;
-            countryInfo.innerHTML =""
+        fetchCountries(name) 
+            .then(countries => {
+                if (countries.length > 10) {
+                Notify.info("Too many matches found. Please enter a more specific name.");
+                return;
+            }
+            if (countries.length > 1 && countries.length <= 10) {
+                 list.innerHTML = createCountryListMarkup(countries);
+                 return;
+            } 
+                countryInfo.innerHTML = createCountryMarkup(countries[0]);     
+            })
+            .catch(error => {
+                        if (error.message === '404') {
+                          Notify.failure('Oops, there is no country with that name');
+                        } 
+                        else {
+                          Notify.failure('Something went wrong');
+                        }})
+            }
         }
-    } else {
-    const countryName = country[0].name.official;
-    const flag = country[0].flags.svg;
-    const capital = country[0].capital;
-    const population = country[0].population;
-    const languages = Object.values(country[0].languages);
+    
 
-        list.innerHTML = `<li class="country_header"><img src="${flag}" alt="flag of ${countryName}" width="80">
-    <p class="country_name">${countryName}</p>`;
-        
-        countryInfo.innerHTML = `<p><span class="description_title">Capital:</span> ${capital}</p>
-    <p><span class="description_title">Population:</span> ${population}</p>
-    <p><span class="description_title">Languages:</span> ${languages}</p>`;
-    }
-}
+function createCountryListMarkup(countries) {
+    return countries
+      .map(({ flags, name }) => `<li class='country'>
+        <img class='country-flag' src='${flags.svg}' width="50">
+        <p class='country-name'>${name.official}</p>
+        </li>`
+      )
+      .join('');
+  }
+
+  function createCountryMarkup({ flags, name, capital, population, languages }) {
+    return `<div class='country-header'>
+                          <img class='country-flag' src='${flags.svg}'width="50">
+                          <p class='country-name country-name--large'>${name.official}</p>
+                      </div>
+                      <ul class='meta-info' >
+                          <li class='meta-info__item'><span>Capital:</span> ${capital}</li>
+                          <li class='meta-info__item'><span>Population:</span> ${population}</li>
+                          <li class='meta-info__item'><span>Languages:</span> ${Object.values(
+                languages
+              ).join(', ')}</li>
+                      </ul>
+      `;
+  }
+
+  function cleanInput() {
+    list.innerHTML = "";
+    countryInfo.innerHTML="";
+  }
